eb97b579ab76ff1e087bf68b114675a96ed59736
arielcoral <123407136+arielcoral@users.noreply.github.com>
2023-02-15 18:10:40 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,161 @@
+// משימה ראשונה 
+var number1 = 2
+var number2 = 2
+ if (number1 === number2){
+    console.log("BOOM")
+ }
+ else {
+    console.log("TRY AGAIN")
+ }
+
+ // משימה ראשונה 
+ // משימה שניה  
+
+ var number3 = 7
+ var number4 = 50
+ console.log(number3 + number4)
+
+ // משימה שניה 
+ // משימה שלישית
+ var number5 = 12
+ var number6 = 4
+ console.log(number5 * number6)
+
+ // משימה שלישית
+ // משימה רביעית
+ var day = 6
+
+ if (day === 1){
+ console.log("good week")
+}
+else if (day === 2 || day === 3 || day === 4){
+    console.log("good day")
+}
+else if(day == 5 || day == 6 || day == 7){
+    console.log("happy weekend!")
+}
+else {
+    console.log("TRY AGAIN")
+}
+
+// משימה רביעית
+// משימה חמישית 
+var month = 1
+if (month == 1){
+    console.log("good month")
+}
+else if (month >= 2 && month <10){
+    console.log("start work")
+}
+else if (month == 10){
+    console.log("get salary")
+}
+else if (month >=11 && month <20){
+    console.log("BE HAPPY")
+}
+else if (month >=20 && month <30){
+    console.log("the end")
+}
+else if (month == 31){
+    console.log("BOOM BOOM")
+}
+else{
+    console.log("TRY AGAIN")
+}
+
+// משימה חמישית
+// משימה שישית
+var holiday = "suckot"
+
+if (holiday === "purim"){
+    console.log("happy purim")
+}
+else if (holiday === "passover"){
+    console.log("clear home")
+}
+else if (holiday === "shavoout"){
+    console.log("eat milk")
+}
+else if (holiday === "suckot"){
+    console.log("build sucka")
+}
+// משימה שישית
+// משימה שבע
+var player1_name = "ariel"
+var player1_score = 60  
+
+var player2_name = "noam"
+var player2_score = 80
+
+if (player1_score > player2_score){
+    console.log(player1_name, player1_score)
+}
+else if (player1_score < player2_score){
+    console.log(player2_name, player2_score)
+}
+// משימה שבע 
+// משימה שמונה 
+var number7 = 8
+
+if(number7 % 2==0){
+   console.log("even")
+}
+ else {
+    console.log("ood")
+}
+// משימה שמונה
+// משימה תשע
+var number8 = 100
+var number9 = 30
+
+if (number8 > number9){
+    console.log(number8)
+}
+else if (number8 < number9){
+    console.log(number9)
+}
+// משימה תשע
+// משימה עשר
+var r10 = 77
+var r11 = 5
+var r12 = 10
+var r13 = 30
+var r14 = 7
+var r15 = 9
+var r16 = 53
+console.log((r10/7)+(r11/7)+(r12/7)+(r13/7)+(r14/7)+(r15/7)+(r16/7))
+// משימה עשר 
+// משימה אחת עשרה
+var user_name = "אריאל"
+var user_age = 35
+var user_running = 30
+
+if (user_age >= 30 && user_age <50 && user_running >= 30 && user_running <51){
+    console.log("אתה בכושר מעולה", user_name, user_age)
+}
+else if (user_age >= 30 && user_age <50 && user_running >= 10 && user_running <30){
+    console.log("אתה בכושר טוב", user_name, user_age)
+}
+else if (user_age >= 30 && user_age <50 && user_running >= 0 && user_running <10){
+    console.log("אתה חייב להשתפר", user_name, user_age)
+}
+else if (user_age >= 18 && user_age <31 && user_running >= 25 && user_running <50){
+    console.log("אתה בכושר טוב", user_name, user_age)
+}
+else if (user_age >= 18 && user_age <31 && user_running < 25){
+    console.log("יש מקום לשיפור", user_name, user_age)
+}
+// משימה אחת עשרה 
+// משימה שתים עשרה
+ var time = 7 
+
+ if (time >= 5 && time <12){
+    console.log("בוקר טוב")
+ }
+ else if (time >= 12 && time <18){
+    console.log("צהריים טובים")
+ }
+ else if (time >= 18 && time <23){
+    console.log("ערב טוב")
+ }
+ //משימה שתים עשרה
